1464426f1b3759577fc97f5aec50e35b5a372be5
zakariabijoy <zakaria.bijoy@live.com>
2021-07-07 23:10:04 +0600

    added order,item,order-item models, component, service -client

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+import { CustomerModel } from './shared/customer/customer.model';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderComponent,
+    OrderItemsComponent,
+    CustomerModel
   ],
   imports: [
     BrowserModule,
